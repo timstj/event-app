c1652e32f4765d59fffc49fa0c22db5e90e02fe3
timstj <161699701+timstj@users.noreply.github.com>
2025-10-14 21:00:53 +0200

    Created a seedUser script with faker-js library.

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -8,15 +8,15 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
-pool.on('connect', () => {
-    console.log('Connection pool established with database');
+pool.on("connect", () => {
+  console.log("Connection pool established with database");
 });
 
 export default pool;
---
+++ server/src/seeds/seedUsers.js
@@ -0,0 +1,45 @@
+import { faker } from "@faker-js/faker";
+import pool from "../config/db.js";
+import { slugify } from "../utils/slugify.js";
+import bcrypt from "bcryptjs";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const NUM_USERS = 10; // Number of users to create
+
+async function seedUsers() {
+  for (let i = 0; i < NUM_USERS; i++) {
+    const first_name = faker.person.firstName();
+    const last_name = faker.person.lastName();
+    const email = faker.internet.email({
+      firstName: first_name,
+      lastName: last_name,
+    });
+    const password = await bcrypt.hash("password123", 10); // Default password for all
+    let baseSlug = slugify(first_name, last_name);
+    let uniqueSlug = baseSlug;
+    let count = 1;
+
+    // Ensure slug is unique
+    while (
+      (await pool.query("SELECT 1 FROM users WHERE slug = $1", [uniqueSlug]))
+        .rowCount > 0
+    ) {
+      uniqueSlug = `${baseSlug}-${count++}`;
+    }
+
+    await pool.query(
+      "INSERT INTO users (first_name, last_name, email, password, slug) VALUES ($1, $2, $3, $4, $5)",
+      [first_name, last_name, email, password, uniqueSlug]
+    );
+    console.log(`Created user: ${first_name} ${last_name} (${email})`);
+  }
+  console.log("Seeding complete!");
+  process.exit();
+}
+
+seedUsers().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
